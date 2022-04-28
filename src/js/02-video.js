47ad3774932af5f1b6009580ef6f40631e79a67b
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-04-28 19:08:22 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+
+const iframe = document.querySelector('#vimeo-player')
+import throttle from 'lodash.throttle';
+
+const player = new Vimeo.Player(iframe);
+
+const onTimeUpdate= date => {
+    localStorage.setItem('videoplayer-current-time', date.seconds);
+
+    console.log(parseInt(localStorage.getItem('videoplayer-current-time')))
+};
+
+player.on('timeupdate ', throttle(onTimeUpdate, 1000));
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
